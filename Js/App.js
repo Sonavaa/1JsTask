0ce35d2ec49df54c670f71d7cb99166c50409e68
Sonava <sonava@code.edu.az>
2022-11-15 05:12:10 +0400

    Commit 1

--- Js/App.js
+++ Js/App.js
@@ -7,8 +7,8 @@
 
 
 //#region Dark_Light_Mode
+  var css = document.body.style;
   function Mode(){
-    var css = document.body.style;
     css.transition = "1s";
     
 
@@ -36,6 +36,21 @@ const date = new Date();
 var d = date.getDay();
 var m = date.getMonth();    
 var y = date.getFullYear();
+var h = date.getHours();
+var mi = date.getMinutes();
 var message = "Welcome";
-alert(`${d}:${m}:${y} ${"  "} ${message}`)
+alert(`${d}.${m}.${y}  ${h}:${mi} ${"  "} ${message}`);
+
+console.log(h);
+
+if(date.getHours() > "12") {
+        css.backgroundColor = "black";
+        css.color = "white";
+        document.getElementById("button").innerHTML = "Light mode";
+} 
+else {
+        css.backgroundColor = "white";
+        css.color = "black"; 
+        document.getElementById("button").innerHTML = "Dark mode";
+}
 //#endregion
